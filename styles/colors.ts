d697f8b498a63074a65239f38f872a5c11245a12
Shantanu <kulkarni.shantanu4@gmail.com>
2024-04-09 16:10:01 +0530

    Added green theme

--- styles/colors.ts
+++ styles/colors.ts
@@ -1,3 +1,26 @@
+// const COLORS = {
+//   primary300: '#a57701',
+//   primary500: '#e6a500',
+//   primary700: '#e2a916',
+
+//   secondary300: '#111',
+//   secondary500: '#333',
+//   secondary700: '#666',
+
+//   accent300: '#82002d',
+//   accent500: '#BC0E4C',
+//   accent700: '#fb1d6b',
+//   accent900: '#ff6a9e',
+
+//   bg300: '#0a1713',
+//   bg500: '#0e241e',
+//   bg700: '#0c3f31',
+
+//   text200: '#999',
+//   text300: '#aaa',
+//   text500: '#ddd',
+//   text700: '#fff',
+// };
 const COLORS = {
   primary300: '#b89000',
   primary500: '#e1a900',
